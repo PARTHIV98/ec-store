1b758d1a043e2dcc7bdd640bd4417529a4323130
parth-98 <ppg31998@gmail.com>
2021-01-30 16:38:09 +0530

    updating shop page, added Collection preview and collection item Component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ import "./App.css";
 import HomePage from "./pages/homepage/home.component";
 import Shop from "./pages/shop/Shop";
 
-const HatsPage = () => (
-<div>
-  <h1>Hats Page</h1>
-</div>
-)
+
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+ 
         <Route path="/shop" component={Shop} />
       </Switch>
     </div>
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,12 +1,26 @@
+import { render } from "@testing-library/react";
 import React, { useState } from "react";
+import { Component } from "react";
+import CollectionPreview from "../../components/preview-collection/collection-preview.component";
 import Shop_Data from './shopData'
+
 
 const Shop = () => {
 
-    const [collection, setCollection] = useState({Shop_Data});
+    const [collection, setCollection] = useState(Shop_Data);
     
+     console.log(collection);
+      return(
+         
+        <div className="shop-page">
+            {collection.map(({ id , ...otherCollectionData}) => (
+                    <CollectionPreview key={id} {...otherCollectionData} />
+                ))
+            }
+        </div>
+      )
+
   
-        return <div>SHOP PAGE</div>;
   
     
 }
